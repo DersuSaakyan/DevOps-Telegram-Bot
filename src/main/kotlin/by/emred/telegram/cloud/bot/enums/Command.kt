7297b23599a9e8a9e2a752aa82720a430948173e
Derenik Saakyan <de.saakyan@tbank.ru>
2025-05-17 23:14:09 +0400

    Add command to list all commands

--- src/main/kotlin/by/emred/telegram/cloud/bot/enums/Command.kt
+++ src/main/kotlin/by/emred/telegram/cloud/bot/enums/Command.kt
@@ -1,7 +1,8 @@
 package by.emred.telegram.cloud.bot.enums
 
 enum class Command(val value: String, val description: String) {
-    START("start", ""),
-    PODS("pods", "Get all pods"),
-    NAMESPACE_CHANGE("namespace_change", "Change namespace")
+    START("/start", ""),
+    PODS("/pods", "Get all pods"),
+    NAMESPACE_SET("/namespace_set", "Set namespace"),
+    BUTTONS("/buttons", "Show all available buttons"),
 }
--- src/main/kotlin/by/emred/telegram/cloud/bot/helper/BotUtils.kt
+++ src/main/kotlin/by/emred/telegram/cloud/bot/helper/BotUtils.kt
@@ -1,6 +1,9 @@
 package by.emred.telegram.cloud.bot.helper
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
 
 object BotUtils {
 
@@ -8,4 +11,19 @@ object BotUtils {
         SendMessage(chatId.toString(), text)
             .apply { enableMarkdown(true) }
             .apply { disableWebPagePreview() }
+
+    fun createMessageWithSimpleButtons(chatId: Long, text: String, simpleButtons: List<List<String>>) =
+        createMessage(chatId, text)
+            .apply {
+                replyMarkup = getSimpleKeyboard(simpleButtons)
+            }
+
+    private fun getSimpleKeyboard(allButtons: List<List<String>>): ReplyKeyboard =
+        ReplyKeyboardMarkup().apply {
+            keyboard = allButtons.map { rowButtons ->
+                val row = KeyboardRow()
+                rowButtons.forEach { rowButton -> row.add(rowButton) }
+                row
+            }
+        }
 }
